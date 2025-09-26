f030714eb8df9698ac36afba0f3c35f00630e371
Vitaliya <vitaliya_malyutina@mail.ru>
2025-09-26 17:48:35 +0300

    Немного улучшена логика

--- src/main.js
+++ src/main.js
@@ -5,27 +5,36 @@ const form = $('form[name="form"]');
 const input = $('.input')
 
 const getRandomInt = () => {
-    return Math.floor(Math.random() * 100 + 1)
-}
-
-const handleSubmit = (e) => {
-    e.preventDefault()
+    return Math.floor(Math.random() * 100) + 1
 }
 
 form.addEventListener('submit', (e) => {
-    handleSubmit(e)
+    e.preventDefault()
     isShowResult()
 })
 
 function isShowResult () {
+
+    let count = []
+
     const resultContainer = $('.content__result')
     const inputValue = parseInt(input.value)
     const randomInt = getRandomInt()
 
+    console.log(randomInt)
+
+    const answers = $('.content__answers')
+
     if (randomInt === inputValue) {
-        resultContainer.textContent = 'Вы выиграли!'
-    } else {
-        resultContainer.textContent = 'Вы проиграли :('
+        resultContainer.textContent = 'Вы выиграли'
+    }
+    else if (randomInt > inputValue) {
+        resultContainer.textContent = 'Вы проиграли'
+        answers.textContent = 'Число меньше загаданного'
+
+    } else if (randomInt < inputValue) {
+        resultContainer.textContent = 'Вы проиграли'
+        answers.textContent = 'Число больше загаданного'
     }
 }
 
