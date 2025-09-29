4b894fc6c5c1bcb54241dfabfdc9e240c2e68260
Vitaliya <vitaliya_malyutina@mail.ru>
2025-09-29 21:12:08 +0300

    Добавлена валидация кнопки, добавлено 5 попыток, чтобы угадать число

--- src/main.js
+++ src/main.js
@@ -3,44 +3,71 @@ import './style.css';
 const $ = document.querySelector.bind(document);
 const form = $('form[name="form"]');
 const input = $('.input')
+const restartButton = $('.button-restart')
+
+const resultContainer = $('.content__result')
+const answers = $('.content__answers')
+
+let randomInt
+let attempts = 0
+const maxAttempts = 5
 
 const getRandomInt = () => {
     return Math.floor(Math.random() * 100) + 1
 }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    isShowResult()
-})
+function startNewGame() {
+    randomInt = getRandomInt()
+}
+
+startNewGame()
 
 function isShowResult () {
-
-    let count = []
-
-    const resultContainer = $('.content__result')
+    attempts++
     const inputValue = parseInt(input.value)
-    const randomInt = getRandomInt()
-
-    console.log(randomInt)
-
-    const answers = $('.content__answers')
 
     if (randomInt === inputValue) {
         resultContainer.textContent = 'Вы выиграли'
     }
-    else if (randomInt > inputValue) {
+    else if (attempts === maxAttempts)  {
         resultContainer.textContent = 'Вы проиграли'
+        answers.textContent = ''
+        isValidButton(true)
+    }
+    else if (randomInt > inputValue) {
+        resultContainer.textContent = 'Игра продолжается'
         answers.textContent = 'Число меньше загаданного'
 
     } else if (randomInt < inputValue) {
-        resultContainer.textContent = 'Вы проиграли'
+        resultContainer.textContent = 'Игра продолжается'
         answers.textContent = 'Число больше загаданного'
     }
 }
 
+function isValidButton(boolean) {
+    const button = $('.button')
+    if(button){
+        button.disabled = boolean
+    } else {
+        button.disabled = boolean
+    }
+}
 
+function restartGame() {
+    form.reset()
+    isValidButton(false)
+    resultContainer.textContent = ''
+    answers.textContent = ''
+    attempts = 0
+    startNewGame()
+}
 
+restartButton.addEventListener('click', () => {
+    restartGame()
+})
 
-
-
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    isShowResult()
+})
 
